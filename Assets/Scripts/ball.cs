fb11e845046ebf4ac21cb3abd00622ba21db5449
Juan De Leon <dele17822@uvg.edu.gt>
2019-01-20 14:08:10 -0600

    Ball movement

---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public int speed = 20;
+
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),  0, Input.GetAxis("Vertical"));
+
+        rigidbody.AddForce(movement * speed );
+       
+    }
+}
