e4147b5b4019a024d87e669f9c0490385f6b7a50
Juan De Leon <dele17822@uvg.edu.gt>
2019-01-21 19:16:16 -0600

    Finalizado

---
+++ Assets/cube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour
+{
+    GameObject gameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Z)) {
+            transform.localScale += new Vector3(5, 5, 5) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.X))
+        {
+            transform.localScale -= new Vector3(5, 5, 5) * Time.deltaTime;
+        }
+    }
+}
